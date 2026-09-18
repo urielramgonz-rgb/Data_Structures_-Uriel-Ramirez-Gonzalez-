1afc2bfaade917c85728b96e7ae5320c91f3169c
urielramgonz-rgb <urielramgonz@gmail.com>
2026-09-18 14:06:07 -0600

    Definiendo arreglos y edades

--- mx/edu/utng/RepasoArreglos.java
+++ mx/edu/utng/RepasoArreglos.java
@@ -3,7 +3,17 @@ public class RepasoArreglos{
 
 	public static void main(String[] args) {
 		System.out.println("Manejando Arreglos");
+
+		String[] nombres = new String[10];
+		int edades[] = {20, 21, 22, 23};
+		System.out.println("Tamaño " + nombres.length);
+		System.out.println("Edades " + edades.length);
+		
+		//Indices
+		nombres[0] = "Juan Alberto";
+		nombres[11] = "Juan Felipe";
+
+		System.out.println(nombres[0]);
 	}
-
-
+	
 }
